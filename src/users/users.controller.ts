3b5d0582bf7726fdc6779428cb212c11caadfeaf
eunbin <silverbean201@wemade.com>
2023-02-13 21:26:58 +0900

    users controller, service 추가

---
+++ src/users/users.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpCode, BadRequestException, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { VerifyEmailDto } from '../email/dto/verify-email.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    const { name, email, password } = dto;
+    return this.usersService.createUser(name, email, password);
+  }
+
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    const { signupVerifyToken } = dto;
+
+    return await this.usersService.verifyEmail(signupVerifyToken);
+  }
+
+  @Get()
+  findAll(@Res() res) {
+    const users = this.usersService.findAll();
+    return res.status(200).send(users);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    if (+id < 1) {
+      throw new BadRequestException('id는 0보다 큰 값이어야 합니다.');
+    }
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
